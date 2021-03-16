c51d81bc3bc733119b10474baa4fe314013f5fdb
Jason Talon <jason.talon@radix.com.au>
2021-03-16 21:38:19 +0800

    add script

---
+++ fn_FIND_KEYS_BY_TABLE.sql
@@ -0,0 +1,36 @@
+DECLARE @TABLE_PREFIXES AS NVARCHAR(128) = 'NCA';
+
+;with module_tables as 
+(
+	SELECT b.TABLE_NAME as MODULE_TABLE_NAME, b.COLUMN_NAME, b.ORDINAL_POSITION, b.IS_NULLABLE, b.DATA_TYPE, b.CHARACTER_MAXIMUM_LENGTH, b.NUMERIC_PRECISION, b.NUMERIC_SCALE  FROM CompanyMerge..fn_ALB_CM_Split_v2(@TABLE_PREFIXES,',') a
+	INNER JOIN INFORMATION_SCHEMA.COLUMNS b ON b.TABLE_NAME LIKE a.[Data] + '%'
+)
+,
+module_tables_primary_key as
+(
+	SELECT DISTINCT module_tables.MODULE_TABLE_NAME, 'PRIMARY' as REFERENCE, ss.PrimaryTableName as REFERENCE_TABLE_NAME, module_tables.COLUMN_NAME
+	  FROM [CompanyMerge].[dbo].[Vw_PrimaryForeignRelationshipSummary] [ss]
+	  INNER JOIN module_tables on  [ss].PrimaryFieldName = module_tables.COLUMN_NAME	  
+)
+,
+module_tables_foreign_key as 
+(
+	SELECT DISTINCT module_tables.MODULE_TABLE_NAME, 'FOREIGN' as REFERENCE, ss.ForeignTableName as REFERENCE_TABLE_NAME, module_tables.COLUMN_NAME
+	  FROM [CompanyMerge].[dbo].[Vw_PrimaryForeignRelationshipSummary] [ss]
+	  INNER JOIN module_tables ON ss.ForeignFieldName = module_tables.COLUMN_NAME
+	  WHERE NOT EXISTS 
+		(
+			SELECT NULL 
+			FROM module_tables_primary_key 
+			WHERE
+				ss.ForeignFieldName = module_tables_primary_key.COLUMN_NAME AND
+				ss.ForeignTableName = module_tables_primary_key.REFERENCE_TABLE_NAME
+		)
+ ),
+ z as 
+ (
+	 select * from module_tables_primary_key
+	 union all
+	 select * from module_tables_foreign_key
+ )
+  select distinct * from z order by MODULE_TABLE_NAME, REFERENCE, REFERENCE_TABLE_NAME, COLUMN_NAME
